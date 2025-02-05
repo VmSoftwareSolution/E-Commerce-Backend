ad33f36378fdcf09cbf3a586aaa9a9689740e52c
VictorMMosqueraG <victormmosquerag@gmail.com>
2025-02-05 15:53:06 -0500

    (CONFIGURATION): Add bearer token in the swagger

--- src/main/java/ecommerce/e_commerce/common/config/SwaggerConfig.java
+++ src/main/java/ecommerce/e_commerce/common/config/SwaggerConfig.java
@@ -4,11 +4,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
-@OpenAPIDefinition
+@OpenAPIDefinition(
+    security = {
+        @SecurityRequirement(name = "bearerAuth")
+    }
+)
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT"  
+)
 public class SwaggerConfig {
     
     /**
